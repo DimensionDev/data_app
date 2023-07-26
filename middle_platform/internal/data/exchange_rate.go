ffb89da8f6cf8331fe70c822967529a9b0d53f7a
TecSong <yiko.song.yuchuan@gmail.com>
2023-07-26 11:19:44 +0800

    update file name

--- middle_platform/internal/data/exchange_rate.go
+++ middle_platform/internal/data/exchange_rate.go
@@ -2,7 +2,6 @@ package data
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/go-kratos/kratos/v2/log"
 	v1 "middle_platform/api/exchange_rate/v1"
@@ -22,6 +21,5 @@ func NewRateRepo(data *Data, logger log.Logger) biz.RateRepo {
 }
 
 func (r *rateRepo) ListAll(context.Context, *v1.RateRequest) ([]*biz.Rate, error) {
-	fmt.Print("test2")
 	return nil, nil
 }
