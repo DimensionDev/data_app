1173deff0004edf047ccb02ae977e05ed4f9e11e
TecSong <yiko.song.yuchuan@gmail.com>
2023-08-07 11:34:02 +0800

    update logic for spam collections filter

--- middle_platform/internal/data/nft_transfer.go
+++ middle_platform/internal/data/nft_transfer.go
@@ -340,7 +340,7 @@ func (r *NftTransferRepo) GetHandleNftinfoFromDB(db *sdk.Gateway, req *pb.GetNft
 		group_by += " group by chain,transaction_hash,owner,event_type,block_timestamp"
 	}
 
-	spam_filter_condition := "and contract_address not in (select contract from spam_contracts) "
+	spam_filter_condition := "and contract_address not in (select contract from spam_contracts) and collection_id not in (select collection_id from collections_spam where spam_score>=80) "
 	first_q := "select chain,transaction_hash,owner,event_type,block_timestamp from transfer_nft_filter_index " + str_where + spam_filter_condition + group_by + str_order + str_limit
 	fmt.Println("first_q:", first_q)
 	first_res, err := r.data.data_query(first_q)
