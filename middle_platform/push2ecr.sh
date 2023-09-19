a847dfad87ec370e56ebfda9c97c96361ce68b9a
sunny <sunny@mask.io>
2023-09-19 21:33:32 +0800

    modify push2ecr sh

--- middle_platform/push2ecr.sh
+++ middle_platform/push2ecr.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-TAG=$(git log -1 --format="%cd-%h" --date=short)
 NAME=data_app
 REPO=032664146980.dkr.ecr.us-east-1.amazonaws.com
 # git checkout develop
@@ -9,9 +8,7 @@ REPO=032664146980.dkr.ecr.us-east-1.amazonaws.com
 
 # login first
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $REPO
-docker build -t $NAME:$TAG -f ./Dockerfile .
+docker build -t $NAME -f .
 
-docker tag $NAME:$TAG $REPO/$NAME:$TAG
-docker tag $NAME:$TAG $REPO/$NAME:latest
-docker push $REPO/$NAME:$TAG
+docker tag $NAME:latest $REPO/$NAME:latest
 docker push $REPO/$NAME:latest
